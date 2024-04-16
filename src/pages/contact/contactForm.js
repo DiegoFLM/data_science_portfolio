b1663b126261acd7b0c664ed2d4e4ce52ef2548a
DiegoFLM <diego.ledesma@correounivalle.edu.co>
2024-04-15 22:57:08 -0500

    Changes in design, new icon and head-shot picture. Better style and relative units for CSS.

--- src/pages/contact/contactForm.js
+++ src/pages/contact/contactForm.js
@@ -1,1 +1,29 @@
-console.log("Wat's up bro?")
+function sendData(event){
+    event.preventDefault(); // Prevents the page from refreshing
+    let name = document.getElementById('name_id').value;
+    let email = document.getElementById('email_id').value;
+    let message = document.getElementById('message_id').value;
+
+    let data = {
+        name: name,
+        email: email,
+        message: message
+    }
+
+    let confirmationMessage = document.getElementById('confirmation_message');
+    confirmationMessage.style.display = 'block';
+
+    let dataList = document.getElementById('data_list');
+    dataList.innerHTML = '';
+
+    for (let key in data){
+        let li = document.createElement('li');
+        li.textContent = key + ': ' + data[key];
+        dataList.appendChild(li);
+    }
+
+    // hide the form
+    let form = document.querySelector('form');
+    form.style.display = 'none';
+
+}
