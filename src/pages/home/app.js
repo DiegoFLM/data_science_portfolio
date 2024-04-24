f664851afb1e60862e9c928983d859ae363dc670
DiegoFLM <diego.ledesma@correounivalle.edu.co>
2024-04-23 22:36:08 -0500

    Contact me form connected to server side and interacting with MongoDB Atlas. Show comments button working.

--- src/pages/home/app.js
+++ src/pages/home/app.js
@@ -4,6 +4,7 @@ function fetchComments(){
     fetch('https://jsonplaceholder.typicode.com/comments')
     .then(response => response.json())
     .then(data => {
+        console.log(data);
         const comment = data[0];
         document.getElementById('name-card-header').textContent = comment.name;
         document.getElementById('email-card-header').textContent = comment.email;
